55d2d0568a8c6af5c642668a36161d158bdcc6b7
Sarsum <33085157+Sarsum@users.noreply.github.com>
2021-07-22 10:55:15 +0200

    Switched from jsx to tsx :)

---
+++ src/pages/index.tsx
@@ -0,0 +1,150 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
+import Icon from "@material-ui/core/Icon";
+import Translate from "@docusaurus/Translate";
+
+type Feature = {
+    title: any
+    icon: string
+    description: any;
+}
+
+const features: Feature[] = [
+    {
+        title: (
+            <Translate id="homepage.feature.openSource.title">
+                Free & OpenSource
+            </Translate>
+        ),
+        icon: "code",
+        description: (
+            <Translate
+                id="homepage.feature.openSource.description"
+                values={{
+                    githubLink: (
+                        <a
+                            href="https://github.com/CloudNetService/"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <Translate id="homepage.feature.openSource.githubLinkName">
+                                GitHub organisation
+                            </Translate>
+                        </a>
+                    ),
+                }}
+            >
+                {
+                    "CloudNet is open source and free to use, and this will not change! You can find all our projects in our {githubLink}."
+                }
+            </Translate>
+        ),
+    },
+    {
+        title: <Translate id="homepage.feature.support.title">Support</Translate>,
+        icon: "people",
+        description: (
+            <Translate
+                id="homepage.feature.support.description"
+                values={{
+                    discordLink: (
+                        <a
+                            href="https://discord.cloudnetservice.eu"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            <Translate id="homepage.feature.support.discordLinkName">
+                                Discord Server
+                            </Translate>
+                        </a>
+                    ),
+                }}
+            >
+                {
+                    "If you have any questions regarding CloudNet, you are welcome to join our {discordLink}. If you have any questions regarding CloudNet, you are welcome to join our"
+                }
+            </Translate>
+        ),
+    },
+    {
+        title: <Translate id="homepage.feature.scalable.title">Scalable</Translate>,
+        icon: "trending_up",
+        description: (
+            <Translate id="homepage.feature.scalable.description">
+                CloudNet is horizontally and vertically scalable. This means you can
+                distribute your players across different virtual or dedicated servers to
+                balance the performance.
+            </Translate>
+        ),
+    },
+];
+
+function Feature({ icon, title, description }: Feature) {
+    return (
+        <div className={clsx("col col--4", styles.feature)}>
+            <div className="text--center">
+                <Icon style={{ fontSize: "4rem" }}>{icon}</Icon>
+            </div>
+            <h3 className="text--center">{title}</h3>
+            <p>{description}</p>
+        </div>
+    );
+}
+
+function Home() {
+    const context = useDocusaurusContext();
+    const { siteConfig = {} } = context;
+    return (
+        <Layout
+            title={`CloudNET - The Cloud Network Environment Technology`}
+            description="A modern application that can dynamically and easily deploy Minecraft oriented software."
+        >
+            <header className={clsx("hero hero--primary", styles.heroBanner)}>
+                <div className="container">
+                    <div className="row">
+                        <div className={clsx("col", "col--8", styles.heroText)}>
+                            <h1 className="hero__title">CloudNet&shy;Service</h1>
+                            <p className="hero__subtitle">{siteConfig.tagline}</p>
+                            <div className={styles.buttons}>
+                                <Link
+                                    className={clsx(
+                                        "button button--outline button--secondary button--lg",
+                                        styles.getStarted
+                                    )}
+                                    to={useBaseUrl("docs/3.3/")}
+                                >
+                                    <Translate id="homepage.getStartedButton">
+                                        Get Started
+                                    </Translate>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className={clsx("col", "col--4", styles.heroImage)}>
+                            <img alt="CloudNetService Logo" src="/img/logo.svg" />
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <main>
+                {features && features.length > 0 && (
+                    <section className={styles.features}>
+                        <div className="container">
+                            <div className="row">
+                                {features.map((props, idx) => (
+                                    <Feature key={idx} {...props} />
+                                ))}
+                            </div>
+                        </div>
+                    </section>
+                )}
+            </main>
+        </Layout>
+    );
+}
+
+export default Home;
---
+++ src/pages/privacy.tsx
@@ -0,0 +1,384 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import styles from "./styles.module.css";
+
+function Home() {
+    return (
+        <Layout title={`Datenschutzerklärung`}>
+            <main className={clsx("container", styles.legalText)}>
+                <h1>Datenschutzerklärung</h1>
+                <p>
+                    Verantwortlicher im Sinne der Datenschutzgesetze, insbesondere der
+                    EU-Datenschutzgrundverordnung (DSGVO), ist:
+                </p>
+                <p>
+                    Joschua Kreimer
+                    <br />
+                    c/o nexserv GmbH
+                    <br />
+                    Dießemer Bruch 112c
+                    <br />
+                    47805 Krefeld
+                    <br />
+                    Deutschland
+                </p>
+                <h2>Ihre Betroffenenrechte</h2>
+                <p>
+                    Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten
+                    können Sie jederzeit folgende Rechte ausüben:
+                </p>
+                <ul>
+                    <li>
+                        Auskunft über Ihre bei uns gespeicherten Daten und deren
+                        Verarbeitung (Art. 15 DSGVO),
+                    </li>
+                    <li>
+                        Berichtigung unrichtiger personenbezogener Daten (Art. 16 DSGVO),
+                    </li>
+                    <li>Löschung Ihrer bei uns gespeicherten Daten (Art. 17 DSGVO),</li>
+                    <li>
+                        Einschränkung der Datenverarbeitung, sofern wir Ihre Daten aufgrund
+                        gesetzlicher Pflichten noch nicht löschen dürfen (Art. 18 DSGVO),
+                    </li>
+                    <li>
+                        Widerspruch gegen die Verarbeitung Ihrer Daten bei uns (Art. 21
+                        DSGVO) und
+                    </li>
+                    <li>
+                        Datenübertragbarkeit, sofern Sie in die Datenverarbeitung
+                        eingewilligt haben oder einen Vertrag mit uns abgeschlossen haben
+                        (Art. 20 DSGVO).
+                    </li>
+                </ul>
+                <p>
+                    Sofern Sie uns eine Einwilligung erteilt haben, können Sie diese
+                    jederzeit mit Wirkung für die Zukunft widerrufen.
+                </p>
+                <p>
+                    Sie können sich jederzeit mit einer Beschwerde an eine
+                    Aufsichtsbehörde wenden, z. B. an die zuständige Aufsichtsbehörde des
+                    Bundeslands Ihres Wohnsitzes oder an die für uns als verantwortliche
+                    Stelle zuständige Behörde.
+                </p>
+                <p>
+                    Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen Bereich)
+                    mit Anschrift finden Sie unter:{" "}
+                    <a
+                        href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html"
+                        target="_blank"
+                        rel="nofollow noopener noreferrer"
+                    >
+                        https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html
+                    </a>
+                    .
+                </p>
+                <p />
+
+                <h2>Erfassung allgemeiner Informationen beim Besuch unserer Website</h2>
+                <h3>Art und Zweck der Verarbeitung:</h3>
+                <p>
+                    Wenn Sie auf unsere Website zugreifen, d.h., wenn Sie sich nicht
+                    registrieren oder anderweitig Informationen übermitteln, werden
+                    automatisch Informationen allgemeiner Natur erfasst. Diese
+                    Informationen (Server-Logfiles) beinhalten etwa die Art des
+                    Webbrowsers, das verwendete Betriebssystem, den Domainnamen Ihres
+                    Internet-Service-Providers, Ihre IP-Adresse und ähnliches.{" "}
+                </p>
+                <p>Sie werden insbesondere zu folgenden Zwecken verarbeitet:</p>
+                <ul>
+                    <li>
+                        Sicherstellung eines problemlosen Verbindungsaufbaus der Website,
+                    </li>
+                    <li>Sicherstellung einer reibungslosen Nutzung unserer Website,</li>
+                    <li>Auswertung der Systemsicherheit und -stabilität sowie</li>
+                    <li>zur Optimierung unserer Website.</li>
+                </ul>
+                <p>
+                    Wir verwenden Ihre Daten nicht, um Rückschlüsse auf Ihre Person zu
+                    ziehen. Informationen dieser Art werden von uns ggfs. anonymisiert
+                    statistisch ausgewertet, um unseren Internetauftritt und die
+                    dahinterstehende Technik zu optimieren.{" "}
+                </p>
+                <h3>Rechtsgrundlage und berechtigtes Interesse:</h3>
+                <p>
+                    Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. f DSGVO auf Basis
+                    unseres berechtigten Interesses an der Verbesserung der Stabilität und
+                    Funktionalität unserer Website.
+                </p>
+                <h3>Empfänger:</h3>
+                <p>
+                    Empfänger der Daten sind ggf. technische Dienstleister, die für den
+                    Betrieb und die Wartung unserer Webseite als Auftragsverarbeiter tätig
+                    werden.
+                </p>
+                <p />
+                <h3>Speicherdauer:</h3>
+                <p>
+                    Die Daten werden gelöscht, sobald diese für den Zweck der Erhebung
+                    nicht mehr erforderlich sind. Dies ist für die Daten, die der
+                    Bereitstellung der Website dienen, grundsätzlich der Fall, wenn die
+                    jeweilige Sitzung beendet ist.{" "}
+                </p>
+                <p>
+                    {" "}
+                    Im Falle der Speicherung der Daten in Logfiles ist dies nach
+                    spätestens 14 Tagen der Fall. Eine darüberhinausgehende Speicherung
+                    ist möglich. In diesem Fall werden die IP-Adressen der Nutzer
+                    anonymisiert, sodass eine Zuordnung des aufrufenden Clients nicht mehr
+                    möglich ist.
+                </p>
+                <p />
+                <h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
+                <p>
+                    Die Bereitstellung der vorgenannten personenbezogenen Daten ist weder
+                    gesetzlich noch vertraglich vorgeschrieben. Ohne die IP-Adresse ist
+                    jedoch der Dienst und die Funktionsfähigkeit unserer Website nicht
+                    gewährleistet. Zudem können einzelne Dienste und Services nicht
+                    verfügbar oder eingeschränkt sein. Aus diesem Grund ist ein
+                    Widerspruch ausgeschlossen.{" "}
+                </p>
+                <p />
+
+                <h2>Cookies</h2>
+                <p>
+                    Wie viele andere Webseiten verwenden wir auch so genannte „Cookies“.
+                    Bei Cookies handelt es sich um kleine Textdateien, die auf Ihrem
+                    Endgerät (Laptop, Tablet, Smartphone o.ä.) gespeichert werden, wenn
+                    Sie unsere Webseite besuchen.{" "}
+                </p>
+                <p>
+                    Sie können Sie einzelne Cookies oder den gesamten Cookie-Bestand
+                    löschen. Darüber hinaus erhalten Sie Informationen und Anleitungen,
+                    wie diese Cookies gelöscht oder deren Speicherung vorab blockiert
+                    werden können. Je nach Anbieter Ihres Browsers finden Sie die
+                    notwendigen Informationen unter den nachfolgenden Links:
+                </p>
+                <ul>
+                    <li>
+                        Mozilla Firefox:{" "}
+                        <a
+                            href="https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen"
+                            target="_blank"
+                            rel="nofollow noopener noreferrer"
+                        >
+                            https://support.mozilla.org/de/kb/cookies-loeschen-daten-von-websites-entfernen
+                        </a>
+                    </li>
+                    <li>
+                        Internet Explorer:{" "}
+                        <a
+                            href="https://support.microsoft.com/de-de/help/17442/windows-internet-explorer-delete-manage-cookies"
+                            target="_blank"
+                            rel="nofollow noopener noreferrer"
+                        >
+                            https://support.microsoft.com/de-de/help/17442/windows-internet-explorer-delete-manage-cookies
+                        </a>
+                    </li>
+                    <li>
+                        Google Chrome:{" "}
+                        <a
+                            href="https://support.google.com/accounts/answer/61416?hl=de"
+                            target="_blank"
+                            rel="nofollow noopener noreferrer"
+                        >
+                            https://support.google.com/accounts/answer/61416?hl=de
+                        </a>
+                    </li>
+                    <li>
+                        Opera:{" "}
+                        <a
+                            href="https://www.opera.com/de/help"
+                            target="_blank"
+                            rel="nofollow noopener noreferrer"
+                        >
+                            https://www.opera.com/de/help
+                        </a>
+                    </li>
+                    <li>
+                        Safari:{" "}
+                        <a
+                            href="https://support.apple.com/kb/PH17191?locale=de_DE&viewlocale=de_DE"
+                            target="_blank"
+                            rel="nofollow noopener noreferrer"
+                        >
+                            https://support.apple.com/kb/PH17191?locale=de_DE&viewlocale=de_DE
+                        </a>
+                    </li>
+                </ul>
+                <h3>Speicherdauer und eingesetzte Cookies:</h3>
+                <p>
+                    Soweit Sie uns durch Ihre Browsereinstellungen oder Zustimmung die
+                    Verwendung von Cookies erlauben, können folgende Cookies auf unseren
+                    Webseiten zum Einsatz kommen:
+                </p>
+                <ul>
+                    <li>theme</li>
+                </ul>
+
+                <h2>Technisch notwendige Cookies </h2>
+                <h3>Art und Zweck der Verarbeitung: </h3>
+                <p>
+                    Wir setzen Cookies ein, um unsere Website nutzerfreundlicher zu
+                    gestalten. Einige Elemente unserer Internetseite erfordern es, dass
+                    der aufrufende Browser auch nach einem Seitenwechsel identifiziert
+                    werden kann.
+                </p>
+                <p>
+                    Der Zweck der Verwendung technisch notwendiger Cookies ist, die
+                    Nutzung von Websites für die Nutzer zu vereinfachen. Einige Funktionen
+                    unserer Internetseite können ohne den Einsatz von Cookies nicht
+                    angeboten werden. Für diese ist es erforderlich, dass der Browser auch
+                    nach einem Seitenwechsel wiedererkannt wird.
+                </p>
+                <p>Für folgende Anwendungen benötigen wir Cookies:</p>
+                <ul>
+                    <li>Übernahme von Designeinstellungen</li>
+                </ul>
+                <h3>Rechtsgrundlage und berechtigtes Interesse: </h3>
+                <p>
+                    Die Verarbeitung erfolgt gemäß Art. 6 Abs. 1 lit. f DSGVO auf Basis
+                    unseres berechtigten Interesses an einer nutzerfreundlichen Gestaltung
+                    unserer Website.
+                </p>
+                <h3>Empfänger: </h3>
+                <p>
+                    Empfänger der Daten sind ggf. technische Dienstleister, die für den
+                    Betrieb und die Wartung unserer Website als Auftragsverarbeiter tätig
+                    werden.
+                </p>
+                <p />
+                <h3>Bereitstellung vorgeschrieben oder erforderlich:</h3>
+                <p>
+                    Die Bereitstellung der vorgenannten personenbezogenen Daten ist weder
+                    gesetzlich noch vertraglich vorgeschrieben. Ohne diese Daten ist
+                    jedoch der Dienst und die Funktionsfähigkeit unserer Website nicht
+                    gewährleistet. Zudem können einzelne Dienste und Services nicht
+                    verfügbar oder eingeschränkt sein.
+                </p>
+                <h3>Widerspruch</h3>
+                <p>
+                    Lesen Sie dazu die Informationen über Ihr Widerspruchsrecht nach Art.
+                    21 DSGVO weiter unten.
+                </p>
+                <p />
+
+                <h2>Eingebettete YouTube-Videos</h2>
+                <p>
+                    Auf unserer Website betten wir YouTube-Videos ein. Betreiber der
+                    entsprechenden Plugins ist die YouTube, LLC, 901 Cherry Ave., San
+                    Bruno, CA 94066, USA (nachfolgend „YouTube“). Die YouTube, LLC ist
+                    einer Tochtergesellschaft der Google LLC, 1600 Amphitheatre Pkwy,
+                    Mountain View, CA 94043, USA (nachfolgend „Google“). Wenn Sie eine
+                    Seite mit dem YouTube-Plugin besuchen, wird eine Verbindung zu Servern
+                    von YouTube hergestellt. Dabei wird YouTube mitgeteilt, welche Seiten
+                    Sie besuchen. Wenn Sie in Ihrem YouTube-Account eingeloggt sind, kann
+                    YouTube Ihr Surfverhalten Ihnen persönlich zuzuordnen. Dies verhindern
+                    Sie, indem Sie sich vorher aus Ihrem YouTube-Account ausloggen.{" "}
+                </p>
+                <p>
+                    Wird ein YouTube-Video gestartet, setzt der Anbieter Cookies ein, die
+                    Hinweise über das Nutzerverhalten sammeln.{" "}
+                </p>
+                <p>
+                    Weitere Informationen zu Zweck und Umfang der Datenerhebung und ihrer
+                    Verarbeitung durch YouTube erhalten Sie in den Datenschutzerklärungen
+                    des Anbieters, Dort erhalten Sie auch weitere Informationen zu Ihren
+                    diesbezüglichen Rechten und Einstellungsmöglichkeiten zum Schutze
+                    Ihrer Privatsphäre (
+                    <a href="https://policies.google.com/privacy" rel="noreferrer">
+                        https://policies.google.com/privacy
+                    </a>
+                    ).{" "}
+                </p>
+                <h3>Widerruf der Einwilligung:</h3>
+                <p>
+                    Vom Anbieter wird derzeit keine Möglichkeit für einen einfachen
+                    Opt-out oder ein Blockieren der Datenübertragung angeboten. Wenn Sie
+                    eine Nachverfolgung Ihrer Aktivitäten auf unserer Website verhindern
+                    wollen, widerrufen Sie bitte im Cookie-Consent-Tool Ihre Einwilligung
+                    für die entsprechende Cookie-Kategorie oder alle technisch nicht
+                    notwendigen Cookies und Datenübertragungen. In diesem Fall können Sie
+                    unsere Website jedoch ggfs. nicht oder nur eingeschränkt nutzen.
+                </p>
+                <p />
+
+                <h2>SSL-Verschlüsselung</h2>
+                <p>
+                    Um die Sicherheit Ihrer Daten bei der Übertragung zu schützen,
+                    verwenden wir dem aktuellen Stand der Technik entsprechende
+                    Verschlüsselungsverfahren (z. B. SSL) über HTTPS.
+                </p>
+                <p />
+                <hr />
+                <h2>Information über Ihr Widerspruchsrecht nach Art. 21 DSGVO</h2>
+                <h3>Einzelfallbezogenes Widerspruchsrecht</h3>
+                <p>
+                    Sie haben das Recht, aus Gründen, die sich aus Ihrer besonderen
+                    Situation ergeben, jederzeit gegen die Verarbeitung Sie betreffender
+                    personenbezogener Daten, die aufgrund Art. 6 Abs. 1 lit. f DSGVO
+                    (Datenverarbeitung auf der Grundlage einer Interessenabwägung)
+                    erfolgt, Widerspruch einzulegen; dies gilt auch für ein auf diese
+                    Bestimmung gestütztes Profiling im Sinne von Art. 4 Nr. 4 DSGVO.
+                </p>
+                <p>
+                    Legen Sie Widerspruch ein, werden wir Ihre personenbezogenen Daten
+                    nicht mehr verarbeiten, es sei denn, wir können zwingende
+                    schutzwürdige Gründe für die Verarbeitung nachweisen, die Ihre
+                    Interessen, Rechte und Freiheiten überwiegen, oder die Verarbeitung
+                    dient der Geltendmachung, Ausübung oder Verteidigung von
+                    Rechtsansprüchen.
+                </p>
+                <h3>Empfänger eines Widerspruchs</h3>
+                <p>
+                    Joschua Kreimer
+                    <br />
+                    c/o nexserv GmbH
+                    <br />
+                    Dießemer Bruch 112c
+                    <br />
+                    47805 Krefeld
+                    <br />
+                    Deutschland
+                </p>
+                <hr />
+                <h2>Änderung unserer Datenschutzbestimmungen</h2>
+                <p>
+                    Wir behalten uns vor, diese Datenschutzerklärung anzupassen, damit sie
+                    stets den aktuellen rechtlichen Anforderungen entspricht oder um
+                    Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen,
+                    z.B. bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt
+                    dann die neue Datenschutzerklärung.
+                </p>
+                <h2>Fragen an den Datenschutzbeauftragten</h2>
+                <p>
+                    Wenn Sie Fragen zum Datenschutz haben, schreiben Sie uns bitte eine
+                    E-Mail oder wenden Sie sich direkt an die für den Datenschutz
+                    verantwortliche Person in unserer Organisation:
+                </p>
+                <p>
+                    E-Mail:{" "}
+                    <a href="mailto:dataprotection@cloudnetservice.eu">
+                        dataprotection@cloudnetservice.eu
+                    </a>
+                </p>
+                <p>
+                    <em>
+                        Die Datenschutzerklärung wurde mithilfe der activeMind AG erstellt,
+                        den Experten für{" "}
+                        <a
+                            href="https://www.activemind.de/datenschutz/datenschutzbeauftragter/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            externe Datenschutzbeauftragte
+                        </a>{" "}
+                        (Version #2020-09-30).
+                    </em>
+                </p>
+            </main>
+        </Layout>
+    );
+}
+
+export default Home;
